f0abd1a52347a1591ca7ce4c52a748d52ae5b749
FannyDeback <fannydeback@gmail.com>
2014-08-06 23:28:09 +0200

    Pagination de l'experience

--- application/controllers/c_experience.php
+++ application/controllers/c_experience.php
@@ -1,25 +1,37 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class c_experience extends CI_Controller {
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->helper('url');
+		$this->load->library('pagination');
+		$this->load->model('m_actors');
+	}
 
 	public function index()
 	{
-		$this->load->helper('url');
+		$config['base_url'] = $this->config->base_url("experiences");
+		$config['total_rows'] = $this->m_actors->actors_count();
+		$config['per_page'] = 10;
+		$config["uri_segment"] = 2;
+		$choice = $config["total_rows"] / $config["per_page"];
+		$config["num_links"] = round($choice);
 
-		$this->load->model('m_actors');
-		$data['experiences'] = $this->m_actors->experiences();
-		
-		$this->load->view('v_header');
+		$this->pagination->initialize($config);
+
+		$page = ($this->uri->segment(2)) ? $this->uri->segment(2) : 0;
+
+		$data["experiences"] = $this->m_actors->experiences($config["per_page"], $page);
+		$data["links"] = $this->pagination->create_links();
+
+		$this->load->view('v_header');		
 		$this->load->view('v_experiences',$data);
 		$this->load->view('v_footer');
 	}
 
 	public function experience($id)
 	{
-		$this->load->helper('url');
-
-		$this->load->model('m_actors');
-
 		$experience = $this->m_actors->experienceById($id);
 
 		$this->load->view('v_header');
