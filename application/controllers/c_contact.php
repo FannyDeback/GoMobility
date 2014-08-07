20985ce9d025d0e4941d40dfab1d5b4523107bba
FannyDeback <fannydeback@gmail.com>
2014-08-07 21:22:26 +0200

    Sync Kelly

---
+++ application/controllers/c_contact.php
@@ -0,0 +1,38 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class c_contact extends CI_Controller {
+
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->helper(array('form','url'));
+		$this->load->library('form_validation');
+		$this->load->library('email', $email_config);
+
+	}
+	// envoyer un mail avec gmail
+	public function index(){
+	    $email_config = Array(
+	        'protocol'  => 'smtp',
+	        'smtp_host' => 'ssl://smtp.googlemail.com',
+	        'smtp_port' => '465',
+	        'smtp_user' => 'codinglikeasir@gmail.com',
+	        'smtp_pass' => 'MySirPassword',
+	        'mailtype'  => 'html',
+	        'starttls'  => true,
+	        'newline'   => "\r\n"
+	    );
+	 
+	 	$this->email->clear();
+	 	
+	    $this->email->from('sir@codinglikeasir.com', 'A. Sir');
+	    $this->email->to('john.sir@example.com');
+	 
+	    $this->email->subject('Sire Subject');
+	    $data = array( 'message' => "Hello Sir, you've got mail!");
+	    $email = $this->load->view('email/template', $data, TRUE);
+	 
+	    $this->email->message( $email );
+	    $this->email->send();
+	}
+}
---
+++ application/views/v_contact.php
@@ -0,0 +1,5 @@
+<div id="container">
+	<?php
+	  	echo form_open('c_contact');
+	?>
+</div>
