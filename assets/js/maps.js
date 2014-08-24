b457ea5f1321f67ad87503080deb05ef0211ea08
FannyDeback <fannydeback@gmail.com>
2014-08-24 16:19:17 +0200

    Commentaire maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -3,7 +3,10 @@ var initialize;
 var calculate;
 var direction;
 var geocoder = new google.maps.Geocoder();
-var infowindow = new google.maps.InfoWindow();
+var infowindow = new google.maps.InfoWindow({
+	maxWidth: 150,
+	maxHeight: 150
+});
 
 // Récupère les coordonnées de l'expérience et affiche la map
 function show_itineraire(experience_id)
@@ -39,7 +42,8 @@ function show_marker()
 		type: 'GET',
 		url: 'experience/dixExperienceAjax',
 		dataType: 'json',
-		success: function(data) {
+		success: function(exps) {
+			// Initialisation de la map
 			var map = new google.maps.Map(document.getElementById('map-canvas'), {
 				zoom: 5,
 				center: new google.maps.LatLng(44.759629, 4.562443),
@@ -47,30 +51,39 @@ function show_marker()
 			});
 
 			var processed = 0;
+			// Utile pour stocker toutes les localisations car geocode asynchrone
 			var locations = [];
-			var experiences = data;
 
-			$.each(data, function(i, v) {
-				geocoder.geocode({'address': v.start}, function(results, status) {
+			// Pour chaque experience contenu dans exps
+			$.each(exps, function(i, experience) {
+				// On récupère la localisation de l'expérience (Lat, Lng)
+				geocoder.geocode({'address': experience.start}, function(results, status) {
+					// Si le retour de geocode est OK, on procède au stockage des localisations
 					if (status == google.maps.GeocoderStatus.OK)
 					{
+						// On stock toutes les localisations
 						locations[i] = results[0].geometry.location;
 					}
 					if (++processed >= locations.length)
 					{
+						// Création d'un nouveau marker avec le titre de l'expérience
 						marker = new google.maps.Marker({
 							position: locations[i],
 							map: map,
-							title: experiences[i].titre
+							title: exps[i].titre
 						});
 
+						/**
+						** Ajout d'un évenement click sur le marker
+						** Lorsque l'on clique sur le marker, on remplit le content de l'info bulle (infowindow)
+						** Puis elle est ouverte
+						*/
 						google.maps.event.addListener(marker, 'click', (function(marker, i) {
 							return function() {
-								content = '<div>'+
-										experiences[i].description+
-										'</div>'+
-										'<div>'+
-										'<a href="experience/'+experiences[i].id+'">Lire la suite</a>'+
+								content = '<div style="width:150px;height:150px">'+
+										'<h2>'+exps[i].titre+'</h2>'+
+										exps[i].description+
+										'<br/><a href="experience/'+exps[i].id+'">Lire la suite</a>'+
 										'</div>';
 								infowindow.setContent(content);
 								infowindow.open(map, marker);
@@ -78,9 +91,7 @@ function show_marker()
 						})(marker, i));
 					}
 				});
-
 			});
-
 		}
 	});
 }
