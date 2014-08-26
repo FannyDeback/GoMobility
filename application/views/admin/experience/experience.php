30a9b04b424e415592fb95f08ca94a8ceb500bbb
kellybreton <kellybreton@MacBook-Pro-de-Kelly.local>
2014-08-26 15:12:53 +0200

    Finition experience, pop in

--- application/views/admin/experience/experience.php
+++ application/views/admin/experience/experience.php
@@ -150,14 +150,14 @@
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Fermer</span></button>
-							<h4 class="modal-title" id="myModalLabel">Mettre à jour une experience</h4>
+							<h2 class="modal-title" id="myModalLabel">Mettre à jour une experience</h2>
 						</div>
 						<div class="modal-body">
-							Mettre à jour l'experience n°<?php echo $experience->id; ?>
+							Mettre à jour l'experience : "<i><?php echo substr($experience->titre, 0, 30); ?> ...</i>"
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-							<button type="submit" class="btn btn-primary">Modifier</button>
+							<button type="submit" class="btn btn-warning">Modifier</button>
 						</div>
 					</div>
 				</div>
