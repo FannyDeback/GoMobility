612192f97a2191aeadf0082c7677ce0b5d60c9f0
kellybreton <kellybreton@MacBook-Pro-de-Kelly.local>
2014-09-01 12:25:22 +0200

    actu

--- application/views/actualite/v_actu.php
+++ application/views/actualite/v_actu.php
@@ -1,5 +1,5 @@
 <div id="col-gauche">
-<h1>Dernière actualité</h1>
+<h1><strong>Dernière</strong> actualité</h1>
 	<?php echo	'<h2>' . $actualite->titre . '</h2>
 				<p>' . $actualite->description . '</p><br/>';?>
 	<?php echo image('11-a.jpg'); ?>
